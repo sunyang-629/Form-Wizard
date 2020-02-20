f3311d9029b7f8f434ca5ac58cf070181f747c79
sunyang9876 <sunyang9876@hotmail.com>
2020-02-20 20:16:17 +1000

    check required

--- script.js
+++ script.js
@@ -13,41 +13,36 @@ const personalDetails = {
     postcode: "",
 } 
 
-// const $$ = id => document.getElementById(id);
+const $$ = id => document.getElementById(id);
 const $ = className => document.getElementsByClassName(className);
 
+// const handlePage = n => {
+//         $('form--stage')[completedSteps].className = $('form--stage')[completedSteps].className.replace('display', '');
+//         completedSteps += n;
+//         $('form--stage')[completedSteps].className += 'display';
+// }
 const handlePage = n => {
+    if (validator()) {
         $('form--stage')[completedSteps].className = $('form--stage')[completedSteps].className.replace('display', '');
         completedSteps += n;
         $('form--stage')[completedSteps].className += 'display';
+    } else {
+        alert('wrong')
+    }
 }
-// const handlePage = n => {
-//     if (validator()) {
-//         $('form--stage')[completedSteps].className = $('form--stage')[completedSteps].className.replace('active', '');
-//         completedSteps += n;
-//         $('form--stage')[completedSteps].className += 'display';
-//     } else {
-//         alert('wrong')
-//     }
-// }
 
-// const validator = () => {
-//     const inputs = $('form--stage')[completedSteps].getElementsByTagName('input');
-//     for (let i = 0; i < inputs.length; i++){
-//         if (inputs[i].required && inputs[i].value === "") {
-//             return false;
-//         }
-//         // } else {
-//         //     console.log(inputs[i]);
-//         //     switch (inputs[i].name) {
-//         //         case 'postcode':
-//         //             console.log(inputs[i]);
-//         //             if (inputs[i].value < 0800 || inputs[i].value > 7999) {
-//         //                 return false;
-//         //             }
-//         //     }
-//         // }
-//         // return true;
-//     }
-//     console.log(inputs);
-// }
+const validator = () => {
+    const inputs = $('form--stage')[completedSteps].getElementsByTagName('input');
+    for (let i = 0; i < inputs.length; i++){
+        if (inputs[i].required && checkIsEmpty(inputs[i].value)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const checkIsEmpty = value => {
+    if (value.trim() === '') {
+        return true
+    } return false
+}
