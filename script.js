2223bbf1c927451fed86ed1dea0345420a8d2254
sunyang9876 <sunyang9876@hotmail.com>
2020-02-20 14:57:55 +1000

    set initial state

---
+++ script.js
@@ -0,0 +1,14 @@
+let completedSteps = 0;
+let remainingSteps = 2;
+const personalDetails = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    streetNumber: "",
+    streetName: "",
+    streetType: "",
+    suburb: "",
+    postcode: "",
+} 
+
